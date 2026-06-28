895de97dc8946aa4951669ad49e127501ed37005
ZeroconZ <50770389+ZeroconZ@users.noreply.github.com>
2026-06-28 11:18:30 +0200

    cambio menor en prueba accion P

--- Scripts_Testbench/P_TB.v
+++ Scripts_Testbench/P_TB.v
@@ -153,14 +153,14 @@ module P_TB;
         clk_datos = 0;
         ini_fin = 0;
         bit_entrada = 0;
-        Uc = 16'd21845;
+        Uc = 16'd16384;
 
         #100;
         reset = 0;
         #100;
 
         $display("-----------------------------------------------------------------");
-        Feedback = -16'd21846;
+        Feedback = 16'd384;
         $display("[%0t] Envio de un valor: %d", $time, Feedback);
         
         test_ACC_P(Feedback); 
